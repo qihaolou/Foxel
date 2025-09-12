801ff26cc76ccb1812e3d464dfad21e0e88247f9
shiyu <im@shiyu.dev>
2025-09-12 20:02:21 +0800

    chore: update version to v1.2.3

--- services/config.py
+++ services/config.py
@@ -4,7 +4,7 @@ from typing import Any, Optional, Dict
 from dotenv import load_dotenv
 from models.database import Configuration
 load_dotenv(dotenv_path=".env")
-VERSION = "v1.2.2"
+VERSION = "v1.2.3"
 
 class ConfigCenter:
     _cache: Dict[str, Any] = {}
