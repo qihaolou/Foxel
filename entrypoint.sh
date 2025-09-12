db2e02dd32aaa1c15ede9b3074c27c6ee1dbdf7b
shiyu <im@shiyu.dev>
2025-09-12 12:06:51 +0800

    chore: Reduce gunicorn worker count

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 python migrate/run.py
 nginx -g 'daemon off;' &
-exec gunicorn -k uvicorn.workers.UvicornWorker -w 4 -b 0.0.0.0:8000 main:app
+exec gunicorn -k uvicorn.workers.UvicornWorker -w 2 -b 0.0.0.0:8000 main:app
